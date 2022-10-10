6afbd06e1664b22d3df0d8f265287f1df709e39e
PietroMil <101657381+PietroMil@users.noreply.github.com>
2022-10-10 12:48:13 +0200

    aggiunta pag utente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { UserComponent } from './pages/utente/utente.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
-  { path: '',   redirectTo: '/login', pathMatch: 'full' },
+  { path: 'utente', component: UserComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: LoginComponent },
 ];
 
---
+++ src/app/pages/utente/utente.component.ts
@@ -0,0 +1,44 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-utente',
+    templateUrl: './utente.component.html',
+    styleUrls: ['./utente.component.scss']
+})
+export class UserComponent {
+    jsonIn = {
+        userType: 'PersonaGiuridica',
+        name: '',
+        surname: '',
+        ragioneSociale: '',
+        indirizzo: {
+            via: '',
+            numeroCivico: 0,
+            cap: 0,
+            comune: '',
+            provincia: '',
+            stato: ''
+        },
+        email: '',
+        telefono: '',
+        pec: '',
+        fax: '',
+        maggiorenne: false,
+        prodottiInteresse: '',
+        sesso: '',
+        partitaIva: '',
+        codiceFiscale: '',
+        rememberMe: false
+    }
+    @ViewChild('utente') utente!: NgForm;
+    showErrors = false;
+
+
+    constructor(private router: Router) { }
+
+
+
+
+}
