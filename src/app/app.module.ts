623910b31911feb0eeaafa2992813ebbf236b2ce
alessandrozaza <alessandro.zaza@edu.itspiemonte.com>
2022-11-30 13:03:45 +0100

    add check-color, fix bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-
-import { BlockUIModule } from 'ng-block-ui';
-import { BlockUIHttpModule } from 'ng-block-ui/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './pages/home/home.component';
 import { DrinkComponent } from './pages/drink/drink.component';
-import { SearchComponent } from './pages/search/search.component';
-import { CardDrinkComponent } from './shared/card-drink/card.drink.component';
+import { IngredientsComponent } from './pages/ingredient/ingredient.component';
+import { OrdiniComponent } from './pages/order/order.component';
+import { CartItemComponent } from './shared/cartItem/cartItem.component';
+import { BlockUIModule } from 'ng-block-ui';
+import { BlockUIHttpModule } from 'ng-block-ui/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     HomeComponent,
     DrinkComponent,
-    SearchComponent,
-    CardDrinkComponent,
+    IngredientsComponent,
+    OrdiniComponent,
+    CartItemComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
     AppRoutingModule,
+    HttpClientModule,
     NgbModule,
     FormsModule,
-    BlockUIModule.forRoot(),
     BlockUIHttpModule.forRoot(),
+    BlockUIModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
